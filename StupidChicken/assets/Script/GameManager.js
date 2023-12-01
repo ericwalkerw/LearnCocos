77966492eeaefb796baba212f744453a0145e515
ericwalkerw <ericwalker@gmail.com>
2023-12-01 17:36:46 +0700

    add explode

--- StupidChicken/assets/Script/GameManager.js
+++ StupidChicken/assets/Script/GameManager.js
@@ -6,6 +6,10 @@ cc.Class({
       car: cc.Node,
       chicken: cc.Node,
       isDone : false,
+      mExplode: cc.Node,
+      _coolDown:0,
+      explodeAnim:cc.Animation,
+      explodeSound:cc.AudioSource,
     },
     update(dt){
         const carCollider = new collider(this.car);
@@ -15,6 +19,19 @@ cc.Class({
             this.isDone = true;
         }
         if(this.isDone){
+            this._coolDown += dt;
+            if(this._coolDown >= 1 && this._coolDown < 1.7){
+                if(this._coolDown <= 1.1){
+                    this.explodeSound.play();
+                }
+                this.mExplode.active = true;
+                this.mExplode.position = this.chicken.position;
+                this.chicken.active = false;
+            }else if(this._coolDown >= 1.7){
+                this.mExplode.active = false;
+            }
+            
+            // this.explodeAnim.play('explode');
             this.chicken.angle += 4;
             this.chicken.position = cc.v2(this.chicken.x + 3,this.chicken.y + 3,0);
         }
